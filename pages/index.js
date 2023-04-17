a0a2b680c6f6901773424b5795cf14f65c3e9c6e
Muhammad Nawfal Mehboob <nawfalmehboob@gmail.com>
2023-04-18 00:55:27 +0500

    WebRTC added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,44 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import requestIp from 'request-ip'
 
 export default function Home({ip}) {
+  const [ipArray, setIpArray] = useState([])
+  useEffect(() => {
+    async function find_public_IP() {
+      //RTCPeerConnection will establish a connection between local and remote host and then returns the connection variable
+      var my_RTC_Peer_Connection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+      var peer_connection = new my_RTC_Peer_Connection({iceServers: [{urls: "stun:stun.l.google.com:19302"}]}), //configurations of peer_connection
+        noop = function() {},
+        local_IP_arr = {},
+        ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+        key;
+
+      function find_ip_locally(ip) {
+        if (!local_IP_arr[ip]) {
+          setIpArray([...ipArray, ip])
+        }
+        local_IP_arr[ip] = true;
+      }
+
+      peer_connection.createDataChannel(""); //create a channel of communication between local and remote host for data transfer
+
+      peer_connection.createOffer(function(sdp) {
+        sdp.sdp.split('\n').forEach(function(line) {
+          if (line.indexOf('candidate') < 0) return;
+          line.match(ip_regex).forEach(find_ip_locally);
+        });
+        peer_connection.setLocalDescription(sdp, noop, noop);
+      }, noop);
+
+      peer_connection.onicecandidate = function(ice) { //event handler
+        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ip_regex)) return;
+        ice.candidate.candidate.match(ip_regex).forEach(find_ip_locally);
+      };
+    }
+
+    find_public_IP()
+  }, [])
   return (
     <>
       <Head>
@@ -12,6 +49,8 @@ export default function Home({ip}) {
       </Head>
       <div>
         {ip}
+        <br />
+        {ipArray}
       </div>
     </>
   )
